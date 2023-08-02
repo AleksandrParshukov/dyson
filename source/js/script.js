b1fa0134a2c274f97b19b23df5cde8801b2d9aaf
Aleksandr <porshe97@yandex.ru>
2023-08-02 22:43:13 +0300

    v0.8

--- source/js/script.js
+++ source/js/script.js
@@ -1,85 +1,41 @@
-$('document').ready(function () {
-	let place = 0;
-	const $leaders_list = $('#leaders_list');
+function init_menu() {
+	const $menu_btns = $('.js_top_menu_btn');
 
-	const $leaders_item = $(
-		'<li class="leaders__item d-flex">' +
-			'<p class="leaders__place"></p>' +
-			'<p class="leaders__name"></p>' +
-			'<p class="leaders__scores"></p>' +
-			'</li>'
-	);
+	$menu_btns.on('click', function (evt) {
+		evt.preventDefault();
 
-	const $participants_item = $(
-		'<li class="participants__item d-flex">' +
-			'<p class="participants__place"></p>' +
-			'<p class="participants__name"></p>' +
-			'<p class="participants__scores"></p>' +
-			'</li>'
-	);
+		$(this).closest('.catalog-select').find('.catalog-select__list').toggleClass('open');
+	});
+}
 
-	$.ajax({
-		url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSdP3ccei1KvSVk2faTVTEmJgRXd7V6KaU0-Wa3qiV1SsqqGcnBuu7A0H_JBIcU9K6sgQ8rZP_yDoG_/pub?output=csv',
-		dataType: 'text',
-		cache: false,
-		success: function (data) {
-			var items = $.csv.toObjects(data);
+function init_faq() {
+	$('.faq__list').accordion({
+		heightStyle: 'content',
+		collapsible: true,
+	});
+	$('.selector').accordion('option', 'header', '.faq__item');
+}
 
-			console.log(items);
-
-			items.sort(function (x, y) {
-				return to_float(y['ИТОГ']) - to_float(x['ИТОГ']);
-			});
-
-			let prev_points = 0;
-
-			$.each(items, function (i, val) {
-				if (prev_points != val['ИТОГ']) {
-					prev_points = val['ИТОГ'];
-					place++;
-				}
-
-				if (i < 3) {
-					add_leaders_item(val);
-				} else if (i < 8) {
-					add_participants_item(val, '1');
-				} else if (i < 15) {
-					add_participants_item(val, '2');
-				} else {
-					add_participants_item(val, '3');
-				}
-			});
+function init_carousel() {
+	$('.js_reviews_carousel').owlCarousel({
+		loop: true,
+		nav: false,
+		autoHeight: false,
+		responsive: {
+			0: {
+				items: 1,
+				dots: false,
+			},
+			420: {
+				items: 2,
+				dots: true,
+			},
 		},
 	});
+}
 
-	function add_leaders_item(data) {
-		let $new_item = $leaders_item.clone();
-
-		$new_item.find('.leaders__place').text(place);
-		$new_item.find('.leaders__name').text(data['Название команды']);
-		$new_item.find('.leaders__scores').text(data['ИТОГ']);
-
-		$leaders_list.append($new_item);
-	}
-
-	function add_participants_item(data, num) {
-		const $participants_list = $('#participants_list' + num),
-			$new_item = $participants_item.clone();
-
-		$new_item.find('.participants__place').text(place);
-		$new_item.find('.participants__name').text(data['Название команды']);
-		$new_item.find('.participants__scores').text(data['ИТОГ']);
-
-		$participants_list.append($new_item);
-	}
-
-	function to_float(num) {
-		return parseFloat(num.replace(/,/, '.'));
-	}
-
-	$(document).on('keydown', function (evt) {
-		if (evt.keyCode == 39) {
-			$('.slide').toggleClass('active');
-		}
-	});
+$('document').ready(function () {
+	init_menu();
+	init_faq();
+	init_carousel();
 });
